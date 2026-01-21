4f1c4b91f5e80c6913acc3b16a4e64c68476870e
Aman Parida <amantechz467@gmail.com>
2026-01-21 20:26:44 +0530

    Font updated using next/google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
+import { Geist, Geist_Mono, Pixelify_Sans, Inter } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -9,6 +9,16 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const GameFont = Pixelify_Sans({
+  variable: "--font-game",
+  subsets: ["latin"],
+});
+
+const inter = Pixelify_Sans({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -25,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased ${GameFont.variable} ${inter.variable}`}
       >
         {children}
       </body>
